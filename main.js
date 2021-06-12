84014e86375ba6f5b38f9095b8d918c0e56919d5
ShreyansSingh <76929035+ShreyansSingh@users.noreply.github.com>
2021-06-11 20:17:35 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+//piggy go fly fly
+
+function bedroom()
+{
+    window.location = "bed.html";
+}
+
+function living()
+{
+    window.location = "livingroom.html";
+}
+
+function balcony()
+{
+    window.location = "balcony.html";
+}
